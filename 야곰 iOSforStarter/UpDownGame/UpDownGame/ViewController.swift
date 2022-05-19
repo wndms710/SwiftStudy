f04a16a43718a83dd61e4e947f550575cd172fba
wndms710 <wndms710@naver.com>
2022-05-19 21:55:18 +0900

    2022-05-19 commit

---
+++ 야곰 iOSforStarter/UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 조주은 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+    print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+}
+
