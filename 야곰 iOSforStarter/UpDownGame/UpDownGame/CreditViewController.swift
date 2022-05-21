26889df035587a4859b08e0fd57a50723e7c09f0
wndms710 <wndms710@naver.com>
2022-05-21 22:56:09 +0900

    2022-05-21 commit

---
+++ 야곰 iOSforStarter/UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 조주은 on 2022/05/21.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
