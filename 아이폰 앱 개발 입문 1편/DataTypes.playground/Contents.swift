9e2772a1d6d011811da0939eb2b01b2d5e00a079
wndms710 <wndms710@naver.com>
2022-05-25 22:29:22 +0900

    2022-05-25 commit

---
+++ 아이폰 앱 개발 입문 1편/DataTypes.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+var message: String = "Hello~🤣"
+//message = 10
+
+var isLogin: Bool = false
+isLogin = true
+
+var day: Int = 27
+
+
+var weight: Double = 65.523452346634567787696
+var height: Float = 175.283764234554
---
+++ 아이폰 앱 개발 입문 1편/Variable.playground/Contents.swift
@@ -0,0 +1,27 @@
+
+var radius = 3.123
+let pi = 3.1415926535
+radius
+radius * radius * pi
+
+radius = 5.456
+radius * radius * pi
+
+//p = 3.15
+
+func cicleArea(r: Double) -> Double {
+    return r * r * 3.1415926535
+}
+
+cicleArea(r: 3.123)
+cicleArea(r: 5.456)
+cicleArea(r: 8.97)
+
+class Circle {
+    var radius = 3.123
+    let pi = 3.1415926535
+    
+    func cicleArea(r: Double) -> Double {
+        return r * r * 3.1415926535
+    }
+}
