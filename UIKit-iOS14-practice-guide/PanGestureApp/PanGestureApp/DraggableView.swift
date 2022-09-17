050917963854365165b5c65ca2c0d02b1db8c588
wndms710 <wndms710@naver.com>
2022-09-18 01:54:01 +0900

    finish PanGesture

--- UIKit-iOS14-practice-guide/PanGestureApp/PanGestureApp/DraggableView.swift
+++ UIKit-iOS14-practice-guide/PanGestureApp/PanGestureApp/DraggableView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DraggableView: UIView {
     
+    var dragType = DragType.none
     
     init() {
         super.init(frame: CGRect.zero)
@@ -18,7 +19,13 @@ class DraggableView: UIView {
         self.addGestureRecognizer(pan)
     }
     
+    // 인터페이스 빌더에서(xib) 만들어 질 때 이 코드가 실행됨
     required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//
+//        let pan = UIPanGestureRecognizer(target: self, action: #selector(dragging))
+//        self.addGestureRecognizer(pan)
+        
         fatalError("init(coder:) has not been implemented")
     }
     
@@ -31,14 +38,32 @@ class DraggableView: UIView {
             let delta = pan.translation(in: self.superview)
             var myPosition = self.center
             
-            myPosition.x += delta.x
-            myPosition.y += delta.y
-            
+            if dragType == .x {
+                myPosition.x += delta.x
+            }
+            else if dragType == .y {
+                myPosition.y += delta.y
+            }
+            else {
+                myPosition.x += delta.x
+                myPosition.y += delta.y
+            }
+
             self.center = myPosition
             pan.setTranslation(CGPoint.zero, in: self.superview)
             
         case .ended, .cancelled:
             print("ended cancelled")
+            // 왼쪽 끝
+            if self.frame.minX < 0 {
+                self.frame.origin.x = 0
+            }
+            // 오른쪽 끝
+            if let hasSuperView = self.superview {
+                if self.frame.maxX > hasSuperView.frame.maxX {
+                    self.frame.origin.x = hasSuperView.frame.maxX - self.bounds.width
+                }
+            }
             
         default:
             break
--- UIKit-iOS14-practice-guide/PanGestureApp/PanGestureApp/ViewController.swift
+++ UIKit-iOS14-practice-guide/PanGestureApp/PanGestureApp/ViewController.swift
@@ -7,20 +7,45 @@
 
 import UIKit
 
+enum DragType {
+    case x
+    case y
+    case none
+}
+
 class ViewController: UIViewController {
+    
+    var dragType = DragType.none
+    let myView = DraggableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myView = DraggableView()
         myView.center = self.view.center
         myView.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        myView.dragType = .x
         myView.backgroundColor = .red
         
         self.view.addSubview(myView)
     }
 
-
+    @IBAction func selectPanType(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            dragType = .x
+        case 1:
+            dragType = .y
+        case 2:
+            dragType = .none
+        default:
+            break
+        }
+        
+        myView.dragType = self.dragType
+        
+    }
+    
 }
 
 
